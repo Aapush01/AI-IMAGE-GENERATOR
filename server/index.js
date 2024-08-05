ef8822be938898ea87031166cb7d8e16d9b5c662
Aapush01 <mdshahidafridia31@gmail.com>
2024-08-05 16:19:29 +0530

    backend init

---
+++ server/index.js
@@ -0,0 +1,24 @@
+import cors from "cors";
+import express from "express";
+import mongoose from "mongoose";
+import * as dotenv from "dotenv";
+import PostRouter from "./routes/Posts.js";
+import GenerateImageRouter from "./routes/GenerateImage.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true }));
+
+// error handler
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || "Something went wrong!";
+  return res.status(status).json({
+    success: false,
+    status,
+    message,
+  });
+});
